bfe4ef64510a462a6510603804a010902f361116
Jessica Heggem <jessicaheggem@gmail.com>
2020-01-29 08:42:53 -0600

    completed assignment

--- joins-solution.sql
+++ joins-solution.sql
@@ -6,7 +6,8 @@ JOIN "addresses" ON "addresses".customer_id = "customers".id;
 -- Get all orders and their line items (orders, quantity and product).
 SELECT "orders".order_date, "line_items".quantity, "products".description 
 FROM "orders"
-JOIN "line_items" ON ....
+JOIN "line_items" ON "orders".id = "line_items".order_id
+JOIN "products" ON "line_items".product_id = "products".id;
 
 -- Which warehouses have cheetos?
 SELECT "warehouse".warehouse from "warehouse"
@@ -28,9 +29,15 @@ SELECT "customers".first_name, "customers".last_name,
 FROM "customers"
 
 -- How many customers do we have?
-
+SELECT COUNT("customers".id)
+FROM "customers";
 
 -- How many products do we carry?
-
+SELECT COUNT("products".id)
+FROM "products";
 
 -- What is the total available on-hand quantity of diet pepsi?
+SELECT SUM("warehouse_product".on_hand)
+FROM "warehouse_product"
+JOIN "products" ON "products".id = "warehouse_product".product_id
+WHERE "products".id = 6;
